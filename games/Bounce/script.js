809ffca6aed0b4c7c243dbf9b8ba8aef179e0dc2
Big Coder <64363860+lalupol@users.noreply.github.com>
2021-06-30 20:45:24 +0000

    Added Bounce game

---
+++ games/Bounce/script.js
@@ -0,0 +1,31 @@
+// Huge credits to https://www.html5rocks.com/en/tutorials/casestudies/gopherwoord-studios-resizing-html5-games/ for the function, I've been searching for a workable one for forever
+
+function resizeGame() {
+  var gameArea = document.getElementById('mycanvas');
+  var widthToHeight = 16 / 9;
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+  var newWidthToHeight = newWidth / newHeight;
+  
+  if (newWidthToHeight > widthToHeight) {
+      newWidth = newHeight * widthToHeight;
+      gameArea.style.height = newHeight + 'px';
+      gameArea.style.width = newWidth + 'px';
+  } else {
+      newHeight = newWidth / widthToHeight;
+      gameArea.style.width = newWidth + 'px';
+      gameArea.style.height = newHeight + 'px';
+  }
+  
+  //gameArea.style.marginTop = (-newHeight / 2) + 'px';
+  //gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+  
+  /*var gameCanvas = document.getElementById('mycanvas');
+  gameCanvas.width = newWidth;
+  gameCanvas.height = newHeight;*/
+}
+
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
+
+//document.body.addEventListener('load', resizeGame);
